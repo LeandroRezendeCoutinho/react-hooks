0ab5a9d84c52fd0b12f3ecb17cc7aeb1d04b79e2
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-02-24 08:56:31 -0300

    use effect to avoid infinity loop

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+function calcFactorial(num) {
+  const n = parseInt(num)
+  if (n < 0) return -1
+  if (n === 0) return 1
+  return calcFactorial(n - 1) * n
+}
+
+function isEven(value) {
+  return value % 2 === 0 ? true : false
+}
 
 const UseEffect = (props) => {
+  const [number, setNumber] = useState(1)
+  const [factorial, setFactorial] = useState(1)
+  const [status, setStatus] = useState("Odd")
+
+  useEffect(function () {
+    setFactorial(calcFactorial(number))
+  }, [number])
+
+  useEffect(function () {
+    setStatus(isEven(number) === true ? "Even" : "Odd")
+  }, [number])
+
   return (
     <div className="UseEffect">
       <PageTitle
         title="Hook UseEffect"
         subtitle="Permite executar efeitos colaterais em componentes funcionais!"
       />
+      <SectionTitle title="Exercise #01" />
+      <div className="center">
+        <div>
+          <span className="text">Fatorial:</span>
+          <span className="text red">{factorial}</span>
+        </div>
+        <input type="number" class="input"
+          value={number}
+          onChange={e => setNumber(e.target.value)} />
+      </div>
+
+      <SectionTitle title="Exercise #02" />
+      <div className="center">
+        <div>
+          <span className="text">Status:</span>
+          <span className="text red">{status}</span>
+        </div>
+      </div>
     </div>
   )
 }
