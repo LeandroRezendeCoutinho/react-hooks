c5b27ec3f11197a8a263ef237664191f51680e8d
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-03-18 08:16:44 -0300

    encapsule context and allow to change defined properties to avoid destroy all context object

--- src/data/Store.jsx
+++ src/data/Store.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialState = {
+  number: 1,
+  text: 'Context API + Hooks'
+}
+
+export const AppContext = React.createContext(initialState)
 
 const Store = props => {
+  const [state, setState] = useState(initialState)
+
+  function updateState(key, value) {
+    setState({
+      ...state,
+      [key]: value
+    })
+  }
   return (
-    <div>{props.children}</div>
+    <AppContext.Provider value={{
+      number: state.number,
+      text: state.text,
+      setNumber: x => updateState('number', x),
+      setText: x => updateState('text', x)
+    }}>
+      {props.children}
+    </AppContext.Provider>
   )
 }
 
--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,29 +1,54 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle';
 import DataContext from "../../data/DataContext";
+import { AppContext } from '../../data/Store';
 
 const UseContext = (props) => {
   const { state, setState } = useContext(DataContext)
 
-  function setNumber(delta) {
+  function addNumber(delta) {
     setState({
       ...state,
       number: state.number + delta
     })
   }
+
+  const { number, text, setNumber, setText } = useContext(AppContext)
+
+  useEffect(function () {
+    if (number > 10) {
+      setText('Maior que 10')
+    }
+  }, [number])
+
   return (
     <div className="UseContext">
       <PageTitle
         title="Hook UseContext"
         subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
       />
+      <SectionTitle title="Exercise #01" />
       <div className="center">
-        <span className="text">{state.number}</span>
         <span className="text">{state.text}</span>
-        <button className="btn" onClick={() => setNumber(-1)}>-1</button>
-        <button className="btn" onClick={() => setNumber(1)}>+1</button>
+        <span className="text">{state.number}</span>
+        <button className="btn" onClick={() => addNumber(-1)}>-1</button>
+        <button className="btn" onClick={() => addNumber(1)}>+1</button>
       </div>
-    </div>
+
+      <SectionTitle title="Exercise #02" />
+      <div className="center">
+        <span className="text">{text}</span>
+        <span className="text">{number}</span>
+        <button className="btn" onClick={() => setNumber(number - 1)}>
+          -1
+        </button>
+        <button className="btn" onClick={() => setNumber(number + 1)}>
+          +1
+        </button>
+
+      </div>
+    </div >
   )
 }
 
