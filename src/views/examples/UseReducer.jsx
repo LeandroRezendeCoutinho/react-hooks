e69a8bc1957445240ef0306f50effc069a272fff
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-03-19 07:48:01 -0300

    add useReducer case examples

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -12,6 +12,14 @@ function reducer(state, action) {
   switch (action.type) {
     case 'number_add2':
       return { ...state, number: state.number + 2 }
+    case 'number_mult7':
+      return { ...state, number: state.number * 7 }
+    case 'number_div25':
+      return { ...state, number: state.number / 25 }
+    case 'number_parseInt':
+      return { ...state, number: parseInt(state.number) }
+    case 'number_addN':
+      return { ...state, number: state.number + action.payload }
     case 'login':
       return { ...state, user: { name: action.payload } }
     default:
@@ -36,6 +44,10 @@ const UseReducer = (props) => {
         <div>
           <button className="btn" onClick={() => dispatch({ type: 'login', payload: 'Mary' })}>Login</button>
           <button className="btn" onClick={() => dispatch({ type: 'number_add2' })}>+2</button>
+          <button className="btn" onClick={() => dispatch({ type: 'number_mult7' })}>*7</button>
+          <button className="btn" onClick={() => dispatch({ type: 'number_div25' })}>/25</button>
+          <button className="btn" onClick={() => dispatch({ type: 'number_parseInt' })}>Int</button>
+          <button className="btn" onClick={() => dispatch({ type: 'number_addN', payload: + 10 })}>+ 10</button>
         </div>
       </div>
     </div>
